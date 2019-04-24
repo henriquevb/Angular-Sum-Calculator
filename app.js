c7ae2a08ee10f29d1680fc622a1e822b45f8bbe0
Henrique Luiz Vieira Barbosa <henriquevb@lnxcit012230bh.cit>
2019-04-24 13:04:29 -0300

    calculadora componentizada

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ myApp.controller('mainController', function($scope) {
         self.resultado = self.parcela1 + self.parcela2;
     }
 });
+
+angular.module('myApp').component('calculadora', {
+    
+    template: '<input name="parcela1" type="number" ng-model="$ctrl.parcela1"/> + <input name="parcela2" type="number" ng-model="$ctrl.parcela2"/><button ng-click="$ctrl.soma()">Soma</button><br>parcela1: {{$ctrl.parcela1}} <br>parcela2: {{$ctrl.parcela2}} <br>resultado: {{$ctrl.resultado}}',
+    bindings: {
+        soma: '&',
+        parcela1: '=',
+        parcela2: '='
+    },
+    replace: true,
+    controller: 'mainController'
+});
