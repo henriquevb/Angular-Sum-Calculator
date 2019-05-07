9e540f0d1ccfed00c8ced4056183a0822620a554
Henrique Luiz Vieira Barbosa <henriquevb@lnxcit012230bh.cit>
2019-05-07 15:05:19 -0300

    Improvements

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let app = angular.module('com.henriquebv.app', [
-    
+
 ]);
  
 const Calculator = function() {
     let self = this;
+    self.parcela1 = 0;
+    self.parcela2 = 0;
+    self.resultado = 0;
+    
 
     self.$onInit = () => {
         console.log('Init calc!!!');
@@ -14,17 +18,49 @@ const Calculator = function() {
         console.log('Called sum()!');
         return self.resultado = self.parcela1 + self.parcela2; 
     };
+
+    self.subtraction = () => {
+        console.log('Called subtraction()!');
+        return self.resultado = self.parcela1 - self.parcela2; 
+    };
 },
-Sum = function() {
+    Division = function () {
+    let div = this;
+
+    div.$onInit = () => {
+        console.log('Init div!!!');
+        console.log(div.name);
+    };
+
+    div.action = () => {
+        console.log('Called division ()!');
+        return div.resultado = div.parcela1 / div.parcela2;
+    };
+},
+ 
+Operation = function() {
     let self = this;
 
     self.$onInit = () => {
-        console.log('Init sum!!!');
+        console.log('Init operation!!!');
     };  
+},
+Multiplication = function () {
+    let mult = this;
+
+    mult.$onInit = () => {
+        console.log('Init mult!!!');
+        console.log(mult.name);
+    };
+
+    mult.action = () => {
+        console.log('Called multiplication ()!');
+        return mult.resultado = mult.parcela1 * mult.parcela2;
+    };
 };
 
-angular.module('com.henriquebv.app',[
-]).component('calculatorComp', {
+angular.module('com.henriquebv.app', ['com.henriquevb.multiplication'])
+.component('calculatorComp', {
     templateUrl: './calculator.html',
     controller: Calculator,
     bindings: {
@@ -32,9 +68,31 @@ angular.module('com.henriquebv.app',[
     }
 }).component('buttonComp', {
     templateUrl: './button.html',
-    controller: Sum,
+    controller: Operation,
     bindings: {
         name: '<',
         action: '&'
     }
+}).component('buttonDivision', {
+    templateUrl: './button.html',
+    controller: Division,
+    bindings: {
+        name: '<',
+        parcela1: '=',
+        parcela2: '=',
+        resultado: '='
+    }
 });
+
+angular.module('com.henriquevb.multiplication', [
+]).component('buttonMultiplication', {
+    templateUrl: './button.html',
+    controller: Multiplication,
+    bindings: {
+        name: '<',
+        parcela1: '=',
+        parcela2: '=',
+        resultado: '='
+    }
+})
+
